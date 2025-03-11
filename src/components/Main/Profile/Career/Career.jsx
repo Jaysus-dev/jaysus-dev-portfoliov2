533c3958ce470f74be025c10de06ea112af94296
Jaysus <edg.estoquia2@gmail.com>
2025-03-11 18:20:47 +0800

    refactored array map career

--- src/components/Main/Profile/Career/Career.jsx
+++ src/components/Main/Profile/Career/Career.jsx
@@ -7,78 +7,46 @@ function Career() {
       <h2 className="section-title">Career Path.</h2>
       <span className="section-subtitle">Evolving every step of the way</span>
       <div className="career__container grid">
-        <div className="career__wrapper ">
-          <h3 className="career__header" id="work">
-            Professional Growth
-          </h3>
-          {Data.career
-            .filter((item) => item.id === "work")
-            .map((item, index, arr) => (
-              <div key={index} className="career__data">
-                <div className="career__marker">
-                  <img
-                    src={item.image}
-                    alt={item.title}
-                    className="career__pic"
-                  />
-                  {index !== arr.length - 1 && (
-                    <span className="career__line"></span>
-                  )}
-                </div>
-                <div className="career__content">
-                  <div className="career__top">
-                    <h3 className="career__title">{item.title}</h3>
-                    <span className="career__date">{item.date}</span>
+        {["work", "academic"].map((sectionId) => (
+          <div className="career__wrapper" key={sectionId}>
+            <h3 className="career__header" id={sectionId}>
+              {sectionId === "work"
+                ? "Professional Growth"
+                : "Academic History"}
+            </h3>
+            {Data.career
+              .filter((item) => item.id === sectionId)
+              .map((item, index, arr) => (
+                <div key={index} className="career__data">
+                  <div className="career__marker">
+                    <img
+                      src={item.image}
+                      alt={item.title}
+                      className="career__pic"
+                    />
+                    {index !== arr.length - 1 && (
+                      <span className="career__line"></span>
+                    )}
                   </div>
-                  <div className="career__bottom">
-                    <span>{item.subtitle}</span>
-                    <p>{item.desc}</p>
-                    <div className="career__skill">
-                      {item.tech?.map((skill, skillIndex) => (
-                        <span key={skillIndex}>{skill.name}</span>
-                      ))}
+                  <div className="career__content">
+                    <div className="career__top">
+                      <h3 className="career__title">{item.title}</h3>
+                      <span className="career__date">{item.date}</span>
+                    </div>
+                    <div className="career__bottom">
+                      <span>{item.subtitle}</span>
+                      <p>{item.desc}</p>
+                      <div className="career__skill">
+                        {item.tech?.map((skill, skillIndex) => (
+                          <span key={skillIndex}>{skill.name}</span>
+                        ))}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
-        </div>
-        <div className="career__wrapper">
-          <h3 className="career__header" id="work">
-            Academic History
-          </h3>
-          {Data.career
-            .filter((item) => item.id === "academic")
-            .map((item, index, arr) => (
-              <div key={index} className="career__data">
-                <div className="career__marker">
-                  <img
-                    src={item.image}
-                    alt={item.title}
-                    className="career__pic"
-                  />
-                  {index !== arr.length - 1 && (
-                    <span className="career__line"></span>
-                  )}
-                </div>
-                <div className="career__content">
-                  <div className="career__top">
-                    <h3 className="career__title">{item.title}</h3>
-                    <span className="career__date">{item.date}</span>
-                  </div>
-                  <div className="career__bottom">
-                    <span>{item.subtitle}</span>
-                    <p>{item.desc}</p>
-                    <div className="career__skill">
-                      {item.tech?.map((skill, skillIndex) => (
-                        <span key={skillIndex}>{skill.name}</span>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-        </div>
+              ))}
+          </div>
+        ))}
       </div>
     </div>
   );
