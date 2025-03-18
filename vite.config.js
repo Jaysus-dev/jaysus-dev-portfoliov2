88301996272dc84bf531066fdff9eb4f076afa61
Jaysus <edg.estoquia2@gmail.com>
2025-03-18 17:32:09 +0800

    fixed route issue

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Ensures proper routing for Vercel
-  server: {
-    port: 3000, // Optional: Define the port for local development
-  },
-  build: {
-    outDir: "dist", // Ensures build output is correctly structured
-  },
 });
