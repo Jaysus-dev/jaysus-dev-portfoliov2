cc747d5aece4de4c3a627d76e7ead785a087fc09
jaysus-dev <edg.estoquia2@gmail.com>
2025-12-05 21:00:20 +0800

    ADDED PHONDE DOCKER PALETTE

--- src/components/Main/Portfolio/Projects/Projects.jsx
+++ src/components/Main/Portfolio/Projects/Projects.jsx
@@ -7,30 +7,36 @@ function Projects() {
     <div className="project section__margin">
       <h2 className="section__title">Recent Work.</h2>
       <span className="section__subtitle">Recent & Selected Projects</span>
+
       <div className="project__container">
         <div className="project__wrapper">
           <div className="project__content grid">
             {Data.projects.map((proj) => (
-              <a href={proj.url} target="_blank" rel="noopener noreferrer">
-                <div key={proj.id} className="project__data">
-                  <video
-                    src={proj.vid}
-                    className="project__video"
-                    autoPlay
-                    loop
-                    muted
-                    playsInline
-                  ></video>
-                  <a href={proj.ur}></a>
-                  <div className="project__info">
-                    <h3>{proj.title}</h3>
-                    <span>{proj.subtitle}</span>
-                    <p>{proj.desc}</p>
-                    <div className="project__stack">
-                      {proj.stack?.map((stack, stackIndex) => (
-                        <p key={stackIndex}>{stack.name}</p>
-                      ))}
-                    </div>
+              <a
+                key={proj.id}
+                href={proj.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project__data"
+              >
+                <video
+                  src={proj.vid}
+                  className="project__video"
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                />
+
+                <div className="project__info">
+                  <h3>{proj.title}</h3>
+                  <span>{proj.subtitle}</span>
+                  <p>{proj.desc}</p>
+
+                  <div className="project__stack">
+                    {proj.stack?.map((stack, index) => (
+                      <p key={index}>{stack.name}</p>
+                    ))}
                   </div>
                 </div>
               </a>
